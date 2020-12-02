1a063d49e4fcc4ae0a3eb44251db412a3be47cbe
Adam Weil <adamweil@Adams-MacBook-Pro.local>
2020-12-02 15:41:21 -0500

    added srollable list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expense_app/transaction.dart';
-import 'package:intl/intl.dart';
-
-import './transaction.dart';
+import './widgets/user_transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,112 +14,40 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: "New Shoes",
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: "New Pant",
-      amount: 109.99,
-      date: DateTime.now(),
-    ),
-  ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                elevation: 5,
-                color: Colors.green,
-                child: Container(
-                  width: double.infinity,
-                  child: Text('CHART'),
-                ),  
-              ),
-            ),
-            Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(decoration: InputDecoration(labelText: 'Title'),),
-                    TextField(decoration: InputDecoration(labelText: 'Amount'),
-                    ),
-                    FlatButton(
-                      child: Text('Add a transaction',),
-                      textColor: Colors.purple,
-                      onPressed: () {},
-                    ),
-                   ],
-                 ),
-              ),
-             ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().add_jm().format(tx.date),
-                            style: TextStyle(color: Colors.grey
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  elevation: 5,
+                  color: Colors.green,
+                  child: Container(
+                    width: double.infinity,
+                    child: Text('CHART'),
                   ),
-                );
-              }).toList(),
-            ),
-          ],
-        ));
+                ),
+              ),
+              UserTransactions()
+            ],
+          
+      ),
+        ),
+        );
   }
 }
 
